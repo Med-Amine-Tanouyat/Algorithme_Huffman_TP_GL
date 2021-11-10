118ee339ff90dc7ef0ca18c9df4baf289078cf15
GHOULAMILYASSE <76974047+GHOULAMILYASSE@users.noreply.github.com>
2021-11-10 23:59:55 +0100

    Update algorithme_de_Huffman.c

--- algorithme_de_Huffman.c
+++ algorithme_de_Huffman.c
@@ -19,3 +19,23 @@ HuffmanTreeNode  *newTreeNode(char c, int freq) {
 	 
 	return t;
 }
+//La structure de l'arbre de Huffman
+typedef struct HuffmanTree {
+    //la capcit� de l'arbre
+    int capacity;
+    //la taille totale de l'arbre de Huffman
+    int size;
+    //Un tableau de pointeurs vers les noeuds de l'arbre
+	HuffmanTreeNode** array;
+} HuffmanTree;
+
+//fonction qui cr�e l'arbre de Huffman � partir d'une capacit� donn�e(allocation m�moire & affectation des champs de strct)
+struct HuffmanTree* createHuffmanTree(int cap) {
+    HuffmanTree* huffmanTree = (HuffmanTree*)malloc(sizeof(HuffmanTree));
+    
+    huffmanTree->size = 0;
+    huffmanTree->capacity = cap;
+    huffmanTree->array = (struct HuffmanTreeNode**)malloc(huffmanTree->capacity * sizeof(struct HuffmanTreeNode*));
+        
+    return huffmanTree;
+}
